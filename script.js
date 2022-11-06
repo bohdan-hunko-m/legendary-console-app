195d3d635ed387e3319c5ba54033bece777a4aa8
Bohdan Hunko <bohdan.hunko.m@gmail.com>
2022-11-06 13:45:51 +0200

    success task 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Задание на урок:
+/* Задание на урок 1:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
 'Сколько фильмов вы уже посмотрели?'
@@ -21,6 +21,20 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+/* Задание на урок 2:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит -
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами */
+
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
@@ -33,12 +47,52 @@ const personalMovieDB = {
   privat: false
 };
 
-const q11 = prompt('Один из последних просмотренных фильмов?', '');
-const q12 = +prompt('На сколько оцените его?', '');
-const q21 = prompt('Один из последних просмотренных фильмов?', '');
-const q22 = +prompt('На сколько оцените его?', '');
+for(let i = 0; i < 2; i++) {
+  const filmName = prompt('Один из последних просмотренных фильмов?', '');
+  const filmRate = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[q11] = q12;
-personalMovieDB.movies[q21] = q22;
+  if(filmName != null && filmRate != null && filmName != '' && filmRate != '' && filmName.length < 50) {
+    personalMovieDB.movies[filmName] = filmRate;
+  } else {
+    i--;
+  }
+}
+
+// let i = 0;
+// while(i < 2) {
+//   const filmName = prompt('Один из последних просмотренных фильмов?', '');
+//   const filmRate = +prompt('На сколько оцените его?', '');
+
+//   if(filmName != null && filmRate != null && filmName != '' && filmRate != '' && filmName.length < 50) {
+//     personalMovieDB.movies[filmName] = filmRate;
+//   } else {
+//     i--;
+//   }
+//   i++;
+// }
+
+// let i = 0;
+// do {
+//   const filmName = prompt('Один из последних просмотренных фильмов?', '');
+//   const filmRate = +prompt('На сколько оцените его?', '');
+
+//   if(filmName != null && filmRate != null && filmName != '' && filmRate != '' && filmName.length < 50) {
+//     personalMovieDB.movies[filmName] = filmRate;
+//   } else {
+//     i--;
+//   }
+//   i++;
+// } while(i < 2)
+
+if(personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if(personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
+
 
 console.log(personalMovieDB);
